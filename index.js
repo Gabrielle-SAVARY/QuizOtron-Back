0dc5332189d854b2bb4fb4885c773ee58e965707
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-04 16:18:06 +0200

    adding express configuration

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ dotenv.config();
 
 /* ---------- Express ---------- */
 const express = require('express');
+const router = require('./app/router');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+/* ---------- Middlewares ---------- */
+app.use(router);
+
+/* ---------- App ---------- */
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} 🚀`);
+})
