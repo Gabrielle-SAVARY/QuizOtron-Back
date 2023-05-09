e8fd1bcaf2f83ad29f47fd5221d3c1e16dd34946
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-09 19:11:10 +0200

    Create: endpoint to get one quiz with all the questions, answers and good answer

--- app/controllers/quizController.js
+++ app/controllers/quizController.js
@@ -5,14 +5,18 @@ const quizController = {
     try {
       const quizzes = await Quiz.findAll({
         include: [
-          {association: 'level'},
+          {
+            association: 'level',
+            attributes: ['name']
+          },
           {
             association: 'author',
-            attributes: ['id', 'pseudo']
+            attributes: ['pseudo']
           },
           {
             association: 'tags',
-          }
+            attributes: ['name']
+          },
         ]
       });
       res.json(quizzes);
@@ -21,6 +25,33 @@ const quizController = {
       res.status(500);
       res.send(error);
     }
+  },
+
+  getOneQuiz: async (req, res) => {
+    const quiz = await Quiz.findByPk(req.params.id, {
+      include: [
+        {
+          association: 'level',
+          attributes: ['name']
+        },
+        {
+          association: 'author',
+          attributes: ['pseudo']
+        },
+        {
+          association: 'tags',
+          attributes: ['name']
+        },
+        {
+          association: 'questions',
+          include: [
+            {association: 'answers'},
+            {association: 'good_answer'}
+          ]
+        }
+      ]
+    });
+    res.json(quiz);
   },
 };
 
--- app/router.js
+++ app/router.js
@@ -4,6 +4,6 @@ const quizController = require('./controllers/quizController');
 
 const router = express.Router();
 
-router.get('/', quizController.getAllQuizzes);
-
+router.get('/quiz', quizController.getAllQuizzes);
+router.get('/quiz/:id', quizController.getOneQuiz);
 module.exports = router;
