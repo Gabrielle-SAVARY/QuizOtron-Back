ac6001b75aefbbd79221d92ec5fdcf1a2d917370
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-10 15:27:06 +0200

    UserController: finishing the request to get the user favorites quizzes

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,44 @@
+const { User } = require("../models");
+const Score = require("../models/score");
+
 const userController = {
   getUserHistory : async (req, res) => {
+    // const favorites = await User.findByPk(1, {
+    //   include: [
+    //     {
+    //       association: 'favorites',
+    //     }
+    //   ]
+    // });
+
+    // res.json(favorites);
+
+    const history = await User.findByPk(2, {
+      include: [
+        {
+          model: Score,
+          as: 'testing',
+        }
+      ]
+    });
+
     
+
+    
+
+    res.json(history);
+  },
+
+  getUserFavorites: async (req, res) => {
+    const favorites = await User.findByPk(3, {
+      include: [
+        {
+          association: 'favorites',
+        }
+      ]
+    });
+
+    res.json(favorites);
   },
 };
 
