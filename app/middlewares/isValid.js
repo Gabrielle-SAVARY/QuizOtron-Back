8c44f27d2253f209313de0bc7897b3abdb8c72b7
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-15 09:42:34 +0200

    Create: Validator for schema to filter allowed data

---
+++ app/middlewares/isValid.js
@@ -0,0 +1,19 @@
+
+// On récupère le schéma et le provider (body, params, query, etc.)
+const isValid = (schema, provider = 'body') => {
+  return function (req, res, next) {
+    // On valide les données reçues du body avec le schéma Joi et on récupère les données validées et les erreurs
+    const { value, error } = schema.validate(req[provider]);
+
+    // Si on a une erreur, on renvoie le message d'erreur
+    if (error) {
+      return res.status(400).json(error.message);
+    }
+
+    // Sinon, on enregistre les données validées dans req.body et on passe au middleware suivant
+    req[provider] = value;
+    next();
+  }
+};
+ 
+module.exports = isValid;
