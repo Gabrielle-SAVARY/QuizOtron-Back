e754e490c252657c0929d83432fa241b0c95cd49
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-18 13:41:07 +0200

    Create: finishing the query to create quiz

--- app/controllers/quizController.js
+++ app/controllers/quizController.js
@@ -1,4 +1,4 @@
-const { Quiz, Tag, Level } = require("../models");
+const { Quiz, Tag, Level, Question, Answer } = require("../models");
 const { Op } = require("sequelize");
 
 const quizController = {
@@ -50,8 +50,7 @@ const quizController = {
           {
             association: 'questions',
             include: [
-              {association: 'answers'},
-              {association: 'good_answer'}
+              {association: 'answers'}
             ]
           }
         ]
@@ -61,6 +60,7 @@ const quizController = {
 
     } catch (error) {
       res.status(500).send(error);
+      console.log(error);
     }
   },
 
@@ -139,6 +139,65 @@ const quizController = {
 
     } catch (error) {
       res.status(500).send(error);
+    }
+  },
+
+  createQuiz: async (req, res) => {
+    try {
+      // On récupère les données du quiz
+      const { title, description, thumbnail, level_id, user_id } = req.body.quiz;
+      console.log('quiz', req.body.quiz);
+
+      // On attribue les données du quiz à un objet newQuiz
+      const newQuiz = {
+        title: title,
+        description: description,
+        thumbnail: thumbnail,
+        level_id: level_id,
+        user_id: user_id,
+      };
+
+      // On crée le quiz en bdd
+      const quiz = await Quiz.create(newQuiz);
+      console.log('quiz', quiz.id);
+
+      // On récupère les données des questions et des réponses
+      const questionsWithAnswers = req.body.questions;
+      // console.log('questions avant le map', questionsWithAnswers);
+
+      // On map sur le tableau de questions et réponses
+      questionsWithAnswers.map(async (questionData) => {
+        const { question, answers } = questionData;
+        console.log('answers récupéré dans le map', answers);
+        console.log('questiondata', questionData)
+
+        // Création de la question
+        const createdQuestion = await Question.create({
+          question: question,
+          quiz_id: quiz.id,
+        });
+        // console.log('question en bdd', JSON.stringify(createdQuestion, null, 2));
+
+        // Création des réponses associées à la question avec un bulkCreate pour créer plusieurs réponses en même temps
+        // On map sur le tableau de réponses
+        const createdAnswers = await Answer.bulkCreate(
+          answers.map((answer) => ({
+            answer: answer.answer,
+            is_valid: answer.is_valid,
+            question_id: createdQuestion.id,
+          }))
+        );
+        console.log('answers en bdd', JSON.stringify(createdAnswers, null, 2));
+      })
+
+
+      res.json({
+        message: "Le quiz a bien été créé",
+      });
+      
+    } catch (error) {
+      res.status(500).send(error);
+      console.log(error);
     }
   },
 };
