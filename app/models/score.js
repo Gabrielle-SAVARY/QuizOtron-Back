cabca986c9c6d2907abecf5d77ac83248c239830
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-25 22:09:49 +0200

    Score model - adding the swagger schema + example

--- app/models/score.js
+++ app/models/score.js
@@ -1,3 +1,25 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Score:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *        quiz_score:
+ *          type: integer
+ *        user_id:
+ *          type: integer
+ *        quiz_id:
+ *          type: integer
+ *      example:
+ *        id: 1
+ *        quiz_score: 10
+ *        user_id: 1
+ *        quiz_id: 1
+ */
+
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../database');
 
