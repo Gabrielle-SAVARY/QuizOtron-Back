db07855241b9bb7ea4a3fc668a74dde71f4cf7d2
SamiBenAbdallah <sami.benabdallah69@gmail.com>
2023-05-25 22:08:54 +0200

    Question model - adding the swagger schema + example

--- app/models/question.js
+++ app/models/question.js
@@ -1,3 +1,22 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Question:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *        question:
+ *          type: string
+ *        quiz_id:
+ *          type: integer
+ *      example:
+ *        id: 1
+ *        question: "Quelle est la couleur du cheval blanc d'Henri IV ?"
+ *        quiz_id: 1
+ */
+
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../database');
 
